261edf2c66aeb13196f8584f7826b09aca332c79
Samuel Heredia <samuel.heredia@bosonit.com>
2021-10-21 14:13:59 +0200

    CRUD Tutorial hasta query

--- src/main/java/com/bosonit/springboot/db1/Controlador.java
+++ src/main/java/com/bosonit/springboot/db1/Controlador.java
@@ -1,9 +1,9 @@
 package com.bosonit.springboot.db1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class Controlador {
@@ -24,8 +24,21 @@ public class Controlador {
         return usuarioRepositorio.findById(id).orElseThrow(() -> new Exception("No encontrado"));
     }
 
+    @Transactional(rollbackFor=Exception.class, propagation = Propagation.NOT_SUPPORTED)
     @PutMapping //UPDATE
     public Usuario editById(@RequestBody Usuario usuario){
+        /*
+        DefaultTransactionDefinition definition = null;
+        TransactionStatus status = null;
+        TransactionManager transactionManager;
+        try{
+            definition = new DefaultTransactionDefinition();
+            status = transactionManager.getTransaction(definition);
+            transactionManager.getTransaction(definition);
+        } catch (Exception k){
+            transactionManager.rollback(status);
+        }
+        */
         System.out.println("Updating");
         usuarioRepositorio.save(usuario);
         return usuario;
@@ -38,6 +51,4 @@ public class Controlador {
         usuarioRepositorio.deleteById(id);
         return ret;
     }
-
-
 }
